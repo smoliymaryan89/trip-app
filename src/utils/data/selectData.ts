76b2ba79b8db7acc918d33142ab60550d9b43503
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-20 21:05:35 +0200

    add select data

---
+++ src/utils/data/selectData.ts
@@ -0,0 +1,70 @@
+export interface SelectData {
+  id: number;
+  city: string;
+  img: string;
+}
+
+const selectData: SelectData[] = [
+  {
+    id: 1,
+    city: "New York",
+    img: "https://res.klook.com/image/upload/Mobile/City/szhx3ytpgfnhpbmsngk0.jpg",
+  },
+  {
+    id: 2,
+    city: "London",
+    img: "https://a.cdn-hotels.com/gdcs/production153/d1371/e6c1f55e-51ac-41d5-8c63-2d0c63faf59e.jpg",
+  },
+  {
+    id: 3,
+    city: "Tokyo",
+    img: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/10/13/tokyo-main.jpg",
+  },
+  {
+    id: 4,
+    city: "Paris",
+    img: "https://assets.cntraveller.in/photos/62752c1974266e7300892c46/1:1/w_3840,h_3840,c_limit/Seine%20paris%20bike-GettyImages-1161606501.jpeg",
+  },
+  {
+    id: 6,
+    city: "Kyiv",
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Lavra_Kyiv.JPG",
+  },
+  {
+    id: 7,
+    city: "Sydney",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Sydney_Opera_House_-_Dec_2008.jpg/800px-Sydney_Opera_House_-_Dec_2008.jpg",
+  },
+  {
+    id: 9,
+    city: "Rio de Janeiro",
+    img: "https://whc.unesco.org/uploads/thumbs/site_1100_0004-750-750-20120625114004.jpg",
+  },
+  {
+    id: 10,
+    city: "Rome",
+    img: "https://media.timeout.com/images/105212586/750/562/image.jpg",
+  },
+  {
+    id: 11,
+    city: "Berlin",
+    img: "https://media.cntraveler.com/photos/5b914e80d5806340ca438db1/1:1/w_2250,h_2250,c_limit/BrandenburgGate_2018_GettyImages-549093677.jpg",
+  },
+  {
+    id: 12,
+    city: "Toronto",
+    img: "https://cdn.getyourguide.com/img/tour/ff7d0f41abd18364.jpeg/145.jpg",
+  },
+  {
+    id: 13,
+    city: "Bangkok",
+    img: "https://a.cdn-hotels.com/gdcs/production172/d459/3af9262b-3d8b-40c6-b61d-e37ae1aa90aa.jpg",
+  },
+  {
+    id: 14,
+    city: "Dubai",
+    img: "https://media.cntraveler.com/photos/646fac649b31895c2ec85c37/4:3/w_1600%2Ch_1200%2Cc_limit/Emirates%2520Offering%2520Free%2520Hotel%2520Stays%2520This%2520Summer%2520to%2520Passengers%2520Flying%2520to%2520Dubai_emma-harrisova-UDsO83Ts6tQ-unsplash.jpg",
+  },
+];
+
+export default selectData;
